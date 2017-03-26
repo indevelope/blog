3fa6a637e29d78c9003d3014d340611df2ac4634
index01d <indevelope@gmail.com>
2017-03-26 18:13:20 +0500

    [refactor][Contacts] controlled component

--- src/components/views/Contacts.js
+++ src/components/views/Contacts.js
@@ -1,9 +1,118 @@
 import React from 'react';
 
-const Contacts = () => (
-  <div>
-    <h1>Contacts</h1>
+import { set, assign } from 'lodash/object';
+
+import classNames from 'classnames';
+
+class Contacts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        values: {
+          fullName: '',
+          email: '',
+          message: ''
+        },
+        errors: {}
+      }
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    alert(JSON.stringify(this.state.form.values));
+  }
+
+  clearError(fieldName) {
+    this.setState(set(
+      assign({}, this.state),
+      ['form', 'errors', fieldName],
+      false
+    ));
+  }
+
+  handleChange(fieldName) {
+    return (e) => {
+      switch(fieldName) {
+        case 'email':
+          this.clearError('email');
+          if (e.target.value.length < 3)
+            this.setState(set(
+              assign({}, this.state),
+              'form.errors.email',
+              true
+            ));
+          break;
+      }
+
+      this.setState(set(
+        assign({}, this.state),
+        ['form', 'values', fieldName],
+        e.target.value
+      ))
+    };
+  }
+
+  render() {
+    const { fullName, email, message } = this.state.form.values;
+    return (
+      <div>
+        <h1>Contacts</h1>
+        <form onSubmit={this.onSubmit} className="ui form">
+          <Text
+            name="fullName"
+            label="Full name"
+            value={fullName}
+            onChange={this.handleChange('fullName')}
+          />
+          <Text
+            name="email"
+            label="Email"
+            value={email}
+            error={this.state.form.errors.email}
+            onChange={this.handleChange('email')}
+          />
+          <TextArea
+            name="message"
+            label="Message"
+            value={message}
+            onChange={this.handleChange('message')}
+          />
+          <input type="submit" value="Submit" className="ui button primary" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Contacts;
+
+const Text = ({ name, value, onChange, label, error }) => (
+  <div className={classNames('ui field', { error })}>
+    <label for={name}>{label}:</label>
+    <input
+      name={name}
+      id={name}
+      className="ui input"
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
   </div>
 );
 
-export default Contacts;
+const TextArea = ({ name, value, onChange, label }) => (
+  <div className="ui field">
+    <label for={name}>{label}:</label>
+    <textarea
+      name={name}
+      id={name}
+      className="ui input"
+      value={value}
+      onChange={onChange}
+    />
+  </div>
+);
